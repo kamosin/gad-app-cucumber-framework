685cbbee04ff794ea25a85093f4258d6e92415c0
Kamil Osinski <kamosin@op.pl>
2024-12-02 23:19:46 +0100

    Dependencies refactor

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -37,8 +37,6 @@ dependencies {
         exclude(module = "snakeyaml")
     }
 
-//    implementation("org.yaml:snakeyaml:2.0")
-
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
--- lib/src/test/java/runner/TestNGApiTestsRunner.java
+++ lib/src/test/java/runner/TestNGApiTestsRunner.java
@@ -2,7 +2,6 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/cucumber/features",
